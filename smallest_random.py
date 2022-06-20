c3041c497c0a2ad6c2fb698a49ff98cedfb7464a
Ubuntu <ubuntu@ip-172-31-61-46.ec2.internal>
2022-06-20 17:53:11 +0000

    fixed

--- smallest_random.py
+++ smallest_random.py
@@ -7,27 +7,41 @@
 import random
 
 
+def smallest(lists):
+    # this function finds the smallest in the list
+    smallest = lists[0]
+    list_counter = 0
+
+    while list_counter < len(lists):
+        if lists[list_counter] < smallest:
+            smallest = lists[list_counter]
+
+        list_counter = list_counter + 1
+
+    return smallest
+
+
 def main():
 
     random_numbers = []
 
     loop_counter = 0
-    smallest = 100
-    # input
+
     print("Here are the 10 numbers:")
 
-    for loop_counter in range(10):
+    while loop_counter < 10:
         random_variable = random.randint(0, 100)
         random_numbers.append(random_variable)
-        if random_variable < smallest:
-            smallest = random_variable
-
         loop_counter = loop_counter + 1
     print("")
-    print(random_numbers)
-    print("The smallest of these 10 numbers is {}".format(smallest))
 
-    print("\n\nDone.")
+    # call function
+    some_var = smallest(random_numbers)
+
+    print(random_numbers)
+    print("\nThe smallest of these 10 numbers is {}".format(some_var))
+
+    print("\nDone.")
 
 
 if __name__ == "__main__":
